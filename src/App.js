c0f3e6e117cf83ad0d9406704fc65bf267e3b03d
Adtiya Kher <adityakher303@gmail.com>
2023-01-31 14:33:21 +0530

    testing complete

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ function App() {
   return (
     <>
     <h1>this is react heading</h1>
-    <h1>this is testing 2 by kushagra</h1>
-    <h1>jaa be lawde</h1>
-    <h1>ase hi bola tha tumnnhe j&k me</h1>
+    
     </>
   );
 }
