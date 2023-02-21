daf816f96894d0d793577a2890795b180d7d7954
Utkarsh Jha <utkarshjhaonline@gmail.com>
2023-02-21 10:32:06 +0530

    this is left card

---
+++ src/components/LeftCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const LeftCard = () => {
+  return (
+    <>
+    <div className="container border-2 w-60 py-4 text-center">
+        <h1>Home</h1>
+        <h1>Trending</h1>
+        <h1>Trending</h1>
+        <h1>Post</h1>
+        
+
+    </div>
+    </>
+  )
+}
+
+export default LeftCard
