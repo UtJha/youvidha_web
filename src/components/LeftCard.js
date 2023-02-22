84f447abe1ccb686e903d2a9f91fcb34b862b4d6
Kushagra Singh <kushagrasinghonline@gmail.com>
2023-02-22 11:50:40 +0530

    commit

--- src/components/LeftCard.js
+++ src/components/LeftCard.js
@@ -1,29 +1,96 @@
-import React from 'react'
+import React from "react";
 
 const LeftCard = () => {
   return (
     <>
-   <div className="flex-col ml-4">
-        
+      <div className="flex-col ml-4">
         <div className="flex-col box-border rounded-lg  w-76 pl-6 pt-6 pb-6 pr-10 border-2 my-4 shadow-2xl">
-        <p className='mt-2 flex'><img src='' alt='img'/><a href="/" className='ml-5'>Home</a></p>
-        <p className='mt-3 flex'><img src='' alt='img'/><a href="/" className='ml-5'>Trending</a></p>
-        <p className='mt-3 flex'><img src='' alt='img'/><a href="/" className='ml-5'>Activity</a></p>
-        <p className='mt-3 flex'><img src='' alt='img'/><a href="/" className='ml-5'>Create</a></p>
+          <p className="mt-3 flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="24"
+              fill="currentColor"
+              class="bi bi-house"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z" />
+            </svg>
+            Home
+          </p>
+          <p className="mt-3 flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="22"
+              fill="currentColor"
+              class="bi bi-graph-up-arrow"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M0 0h1v15h15v1H0V0Zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5Z"
+              />
+            </svg>
+            Trending
+          </p>
+          <p className="mt-3 flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="24"
+              fill="currentColor"
+              class="bi bi-clock-history"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8.515 1.019A7 7 0 0 0 8 1V0a8 8 0 0 1 .589.022l-.074.997zm2.004.45a7.003 7.003 0 0 0-.985-.299l.219-.976c.383.086.76.2 1.126.342l-.36.933zm1.37.71a7.01 7.01 0 0 0-.439-.27l.493-.87a8.025 8.025 0 0 1 .979.654l-.615.789a6.996 6.996 0 0 0-.418-.302zm1.834 1.79a6.99 6.99 0 0 0-.653-.796l.724-.69c.27.285.52.59.747.91l-.818.576zm.744 1.352a7.08 7.08 0 0 0-.214-.468l.893-.45a7.976 7.976 0 0 1 .45 1.088l-.95.313a7.023 7.023 0 0 0-.179-.483zm.53 2.507a6.991 6.991 0 0 0-.1-1.025l.985-.17c.067.386.106.778.116 1.17l-1 .025zm-.131 1.538c.033-.17.06-.339.081-.51l.993.123a7.957 7.957 0 0 1-.23 1.155l-.964-.267c.046-.165.086-.332.12-.501zm-.952 2.379c.184-.29.346-.594.486-.908l.914.405c-.16.36-.345.706-.555 1.038l-.845-.535zm-.964 1.205c.122-.122.239-.248.35-.378l.758.653a8.073 8.073 0 0 1-.401.432l-.707-.707z" />
+              <path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0v1z" />
+              <path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5z" />
+            </svg>
+            Activity
+          </p>
+          <p className="mt-3 flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="24"
+              fill="currentColor"
+              class="bi bi-plus-circle"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+            </svg>
+            Create
+          </p>
         </div>
         <div className=" rounded-lg w-76 pl-6 pt-6 pb-6  pr-10 border-2 shadow-2xl">
-        <div className='flex w-56 box-border border-2 rounded-lg'>
-            <input className='flex-1 h-6 w-20' type="search" name="search-bar" placeholder='search' />
-            <img className='flex-none h-6 w-4' src="" alt="icon" />
+          <div className="flex w-56 box-border border-2 rounded-lg">
+            <input
+              className="flex-1 h-6 w-20"
+              type="search"
+              name="search-bar"
+              placeholder="search"
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="20"
+              fill="currentColor"
+              class="bi bi-search"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+            </svg>
+          </div>
+          <div className=" mt-3 box-border border-2 rounded-lg">
+            <p className="my-2">location</p>
+            <p className="my-2">location</p>
+          </div>
         </div>
-        <div className=' mt-3 box-border border-2 rounded-lg'>
-            <p className='my-2'>location</p>
-            <p className='my-2'>location</p>
-        </div>
-        </div>
-        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default LeftCard
+export default LeftCard;
--- src/components/RightBox.js
+++ src/components/RightBox.js
@@ -1,29 +1,55 @@
-import React from 'react'
+import React from "react";
 
 const RightBox = () => {
   return (
     <>
-        <div className="flex-col mr-4">
-        <div className='flex w-56 mt-3 ml-2 box-border border-2 rounded-lg shadow-2xl'>
-            <input className='flex-1 h-6 w-40' type="search" name="search-bar" placeholder='search' />
-            <img className='flex-none h-6 w-4' src="" alt="icon" />
+      <div className="flex-col mr-4">
+        <div className="flex w-56 mt-3 ml-2 box-border border-2 rounded-lg shadow-2xl">
+          <input
+            className="flex-1 h-6 w-40"
+            type="search"
+            name="search-bar"
+            placeholder="search"
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="20"
+            fill="currentColor"
+            class="bi bi-search"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+          </svg>
         </div>
         <div className="container flex box-border rounded-lg  w-80 p-4 border-2 my-4 shadow-2xl">
-        <a href="/"><img src="" alt="userImage" /></a>
-        <p className='mx-2'>UserName</p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="24"
+            fill="currentColor"
+            class="bi bi-person-circle"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+            <path
+              fill-rule="evenodd"
+              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+            />
+          </svg>
+          <p className="mx-2">UserName</p>
         </div>
         <div className="container rounded-lg w-80 p-4 border-2 shadow-2xl">
-            <h2>What's Happening</h2>
-            <h4 className='my-2'>Content</h4>
-            <hr />
-            <h4 className='my-2'>Content</h4>
-            <hr />
-            <h4 className='my-2'>Content</h4>
+          <h2>What's Happening</h2>
+          <h4 className="my-2">Content</h4>
+          <hr />
+          <h4 className="my-2">Content</h4>
+          <hr />
+          <h4 className="my-2">Content</h4>
         </div>
-        </div>
-        
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default RightBox
+export default RightBox;
